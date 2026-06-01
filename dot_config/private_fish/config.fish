055147b799a4be5ca38fdf61a63578c6cb816fa0
Jiàn <hi@chabuduo.in>
2026-06-01 19:23:57 +0800

    feat: add !! and !$ history abbreviations for fish
    Moves fish_greeting inside the interactive guard and adds
    bash-style !! (last command) and !$ (last argument) abbreviations.

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,6 +1,3 @@
-# Disable the default welcome message when starting a fish shell
-set -g fish_greeting
-
 # Load Homebrew environment variables (PATH, MANPATH, etc.) into fish
 eval (/opt/homebrew/bin/brew shellenv)
 
@@ -9,5 +6,12 @@ eval (/opt/homebrew/bin/brew shellenv)
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
 
 if status is-interactive
-# Commands to run in interactive sessions can go here
+	# Disable the default welcome message when starting a fish shell
+	set -g fish_greeting
+
+	function last_history_item; echo $history[1]; end
+	abbr -a !! --position anywhere --function last_history_item
+
+	function last_history_arg; echo $history[1] | string match -r '[^ ]+$'; end
+	abbr -a !\$ --position anywhere --function last_history_arg
 end
