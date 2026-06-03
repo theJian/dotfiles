01dd35ef5e741c67aa1195e9acc2bf52c2d02ce5
Jiàn <hi@chabuduo.in>
2026-06-03 11:36:00 +0800

    feat: add moonwalk light theme for fish shell

---
+++ dot_config/private_fish/conf.d/moonwalk_theme.fish
@@ -0,0 +1,43 @@
+# Moonwalk light theme for fish shell
+# Generated from nvim-moonwalk palette/light.lua
+
+# --- Syntax Highlighting ---
+set --global fish_color_normal       061F4A
+set --global fish_color_command      0D50C5
+set --global fish_color_keyword      952197
+set --global fish_color_quote        4C6129
+set --global fish_color_redirection  006092
+set --global fish_color_end          952197
+set --global fish_color_error        AF1608
+set --global fish_color_param        061F4A
+set --global fish_color_option       7A5000
+set --global fish_color_comment      7D7E82
+set --global fish_color_operator     494440
+set --global fish_color_escape       00566B
+
+# --- Prompt Elements ---
+set --global fish_color_cwd          0D50C5
+set --global fish_color_cwd_root     AF1608
+set --global fish_color_user         002FA7
+set --global fish_color_host         --reset
+set --global fish_color_host_remote  AF1608
+set --global fish_color_status       AF1608
+set --global fish_color_cancel       --reverse
+
+# --- UI ---
+set --global fish_color_autosuggestion 9C958D
+set --global fish_color_selection      --bold --background=B8D2E5
+set --global fish_color_search_match   --bold --background=FFCF24
+set --global fish_color_history_current --bold
+set --global fish_color_valid_path     --underline=single
+
+# --- Completion Pager ---
+set --global fish_pager_color_progress            145B0E --bold --background=F2EDE9
+set --global fish_pager_color_background          061F4A
+set --global fish_pager_color_prefix              0D50C5 --bold --underline=single
+set --global fish_pager_color_completion          061F4A
+set --global fish_pager_color_description         7D7E82
+set --global fish_pager_color_selected_background B8D2E5
+set --global fish_pager_color_selected_prefix     002FA7 --bold
+set --global fish_pager_color_selected_completion 061F4A
+set --global fish_pager_color_selected_description 7D7E82
